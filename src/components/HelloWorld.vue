35432388d5243733dba6d20788efe9a6095b4255
Dan O <diydan82@gmail.com>
2023-07-07 13:47:28 +0200

    Bolier plate

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <v-img height="300" src="@/assets/logo.svg" />
+
+      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+
+      <h4 class="text-h4 font-weight-bold">Flow Hackathon S2 project</h4>
+
+      <div class="py-14" />
+
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
