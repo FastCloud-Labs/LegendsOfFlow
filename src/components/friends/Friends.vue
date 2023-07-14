bfd7aca7be0b40d0d18a3dd91d43203a49d6dd58
Yash Shah <yashshah192001@gmail.com>
2023-07-14 15:01:59 +0530

    Add invite friends and friends ui

--- src/components/friends/Friends.vue
+++ src/components/friends/Friends.vue
@@ -1,3 +1,38 @@
 <template>
-  <h2>Friends</h2>
+  <div class="friends">
+    <h2>Invite Friends</h2>
+    <v-card class="friends-card">
+      <v-card-title class="d-flex align-center">
+        <v-avatar>
+          <img
+            src="https://res.cloudinary.com/dmovdfcta/image/upload/v1689055906/avatar-default_c8rpgq.png"
+            alt="Avatar"
+          />
+        </v-avatar>
+        <span class="username">John Doe</span>
+        <v-spacer></v-spacer>
+      </v-card-title>
+    </v-card>
+  </div>
 </template>
+
+<style scoped>
+.friends {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.friends-card {
+  padding: 20px;
+  border: none;
+  box-shadow: none;
+}
+
+.username {
+  margin-left: 10px;
+}
+</style>
+
+<script setup>
+import { ref } from "vue";
+</script>
--- src/components/friends/InviteFriends.vue
+++ src/components/friends/InviteFriends.vue
@@ -1,3 +1,60 @@
 <template>
-  <h1>Invite Friends</h1>
+  <div class="invite-friends">
+    <h2>Invite Friends</h2>
+    <v-card class="invite-card">
+      <v-card-title class="d-flex align-center">
+        <v-avatar>
+          <img
+            src="https://res.cloudinary.com/dmovdfcta/image/upload/v1689055906/avatar-default_c8rpgq.png"
+            alt="Avatar"
+          />
+        </v-avatar>
+        <span class="username">John Doe</span>
+        <v-spacer></v-spacer>
+        <div class="button-container">
+          <v-btn class="ma-2 accept-button" color="primary">
+            Add Friend&nbsp;
+          </v-btn>
+          <v-btn class="ma-2 decline-button" color="success">
+            Invite to gaame&nbsp;
+          </v-btn>
+        </div>
+      </v-card-title>
+    </v-card>
+  </div>
 </template>
+
+<style scoped>
+.invite-friends {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.invite-card {
+  padding: 20px;
+  border: none;
+  box-shadow: none;
+}
+
+.username {
+  margin-left: 10px;
+}
+
+.button-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.accept-button {
+  margin: 5px;
+}
+
+.decline-button {
+  margin: 5px;
+}
+</style>
+
+<script setup>
+import { ref } from "vue";
+</script>
