02ff86cb0ff024af6e36e0dea53e850b934d960e
Yash Shah <yashshah192001@gmail.com>
2023-09-04 11:32:45 +0530

    fix username model

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,32 +1,38 @@
 <template>
   <v-layout class="rounded rounded-md fill-height">
     <v-app-bar>
-      <v-img @click="showMyEvents" max-width="200" class="ml-4 hidden-sm-and-down" src="@/assets/logo-horizontal.png"/>
-      <v-img max-width="200" class="ml-4 hidden-md-and-up" src="@/assets/favicon.ico"
-             @click.stop="drawer = !drawer"/>
-      <v-spacer/>
+      <v-img
+        @click="showMyEvents"
+        max-width="200"
+        class="ml-4 hidden-sm-and-down"
+        src="@/assets/logo-horizontal.png"
+      />
+      <v-img
+        max-width="200"
+        class="ml-4 hidden-md-and-up"
+        src="@/assets/favicon.ico"
+        @click.stop="drawer = !drawer"
+      />
+      <v-spacer />
       <v-chip color="success" class="mr-1">
-        <v-icon icon="fas fa-shield-halved" class="mr-1"/>
+        <v-icon icon="fas fa-shield-halved" class="mr-1" />
         XP Points: {{ user.profile?.xpPoints || 0 }}
       </v-chip>
       <v-chip color="info" class="mr-2">
-        <v-icon icon="fas fa-coins" class="mr-1"/>
+        <v-icon icon="fas fa-coins" class="mr-1" />
         LF Tokens: {{ user.profile?.tokens || 0 }}
       </v-chip>
       <v-menu>
-        <template v-slot:activator=" { props }
-      ">
-          <v-btn
-            v-bind="props"
-            size="small"
-            class="ma-0 pa-0 mr-2"
-          >
-            <v-icon icon="fas fa-cog"/>
+        <template v-slot:activator="{ props }">
+          <v-btn v-bind="props" size="small" class="ma-0 pa-0 mr-2">
+            <v-icon icon="fas fa-cog" />
           </v-btn>
         </template>
         <v-list>
           <v-list-item v-if="!user.profile?.dapperAddress">
-            <v-list-item-title @click="showDapper()">Connect Dapper Wallet</v-list-item-title>
+            <v-list-item-title @click="showDapper()"
+              >Connect Dapper Wallet</v-list-item-title
+            >
           </v-list-item>
 
           <v-list-item @click="showDapper()">
@@ -34,24 +40,36 @@
               <v-card-text class="mx-auto text-center">
                 <h3>Account:</h3>
                 <v-avatar class="border ma-2" size="60">
-                  <img :src="profile.avatar"/>
+                  <img :src="profile.avatar" />
                 </v-avatar>
-                <br>
-                <v-chip @click="showDapper()" class="text-truncate" size="small" color="success" variant="outlined">{{
-                    user.profile?.username
-                  }}
+                <br />
+                <v-chip
+                  @click="showDapper()"
+                  class="text-truncate"
+                  size="small"
+                  color="success"
+                  variant="outlined"
+                  >{{ user.profile?.username }}
                 </v-chip>
-                <br>
-                <v-chip v-if="user.profile?.dapperAddress" @click="showDapper()" class="text-truncate mt-2"
-                        size="small" color="primary"
-                        variant="outlined">
+                <br />
+                <v-chip
+                  v-if="user.profile?.dapperAddress"
+                  @click="showDapper()"
+                  class="text-truncate mt-2"
+                  size="small"
+                  color="primary"
+                  variant="outlined"
+                >
                   {{ user.profile?.dapperAddress }}
                 </v-chip>
               </v-card-text>
             </v-card>
           </v-list-item>
           <v-divider></v-divider>
-          <v-list-item v-if="user.profile?.dapperAddress" @click="showMoments()">
+          <v-list-item
+            v-if="user.profile?.dapperAddress"
+            @click="showMoments()"
+          >
             <v-list-item-title>View My Moments</v-list-item-title>
           </v-list-item>
 
@@ -62,51 +80,84 @@
         </v-list>
       </v-menu>
     </v-app-bar>
-    <div class="deskop-menu hidden-sm-and-down fill-height" width="200" height="100%">
+    <div
+      class="deskop-menu hidden-sm-and-down fill-height"
+      width="200"
+      height="100%"
+    >
       <v-list>
         <v-list-item @click="showMyEvents">
-          <v-icon icon="fas fa-calendar-check" class="mr-1 pa-2 ma-2" size="x-small"/>
+          <v-icon
+            icon="fas fa-calendar-check"
+            class="mr-1 pa-2 ma-2"
+            size="x-small"
+          />
           My Games
         </v-list-item>
         <v-list-item @click="showUpcomingEvents">
-          <v-icon icon="fas fa-calendar-days" class="mr-1 pa-2 ma-2" size="x-small"/>
+          <v-icon
+            icon="fas fa-calendar-days"
+            class="mr-1 pa-2 ma-2"
+            size="x-small"
+          />
           Upcoming Events
         </v-list-item>
         <v-list-item @click="showMoments">
-          <v-icon icon="fas fa-address-card" class="mr-1 pa-2 ma-2" size="x-small"/>
+          <v-icon
+            icon="fas fa-address-card"
+            class="mr-1 pa-2 ma-2"
+            size="x-small"
+          />
           Moments
         </v-list-item>
         <v-list-item @click="showLeaderboard">
-          <v-icon icon="fas fa-list-ol" class="mr-1 pa-2 ma-2" size="x-small"/>
+          <v-icon icon="fas fa-list-ol" class="mr-1 pa-2 ma-2" size="x-small" />
           Leaderboards
         </v-list-item>
         <v-list-item @click="showFriendsFoes">
-          <v-icon icon="fas fa-users" class="mr-1 pa-2 ma-2" size="x-small"/>
+          <v-icon icon="fas fa-users" class="mr-1 pa-2 ma-2" size="x-small" />
           Friends & Foes
         </v-list-item>
       </v-list>
     </div>
 
-    <v-navigation-drawer v-model="drawer" location="bottom" temporary class="mobile-menu hidden-md-and-up">
+    <v-navigation-drawer
+      v-model="drawer"
+      location="bottom"
+      temporary
+      class="mobile-menu hidden-md-and-up"
+    >
       <v-list>
         <v-list-item @click="showMyEvents">
-          <v-icon icon="fas fa-calendar-check" class="mr-1 pa-2 ma-2" size="x-small"/>
+          <v-icon
+            icon="fas fa-calendar-check"
+            class="mr-1 pa-2 ma-2"
+            size="x-small"
+          />
           My Games
         </v-list-item>
         <v-list-item @click="showUpcomingEvents">
-          <v-icon icon="fas fa-calendar-days" class="mr-1 pa-2 ma-2" size="x-small"/>
+          <v-icon
+            icon="fas fa-calendar-days"
+            class="mr-1 pa-2 ma-2"
+            size="x-small"
+          />
           Upcoming Events
         </v-list-item>
         <v-list-item @click="showMoments">
-          <v-icon icon="fas fa-address-card" class="mr-1 pa-2 ma-2" size="x-small"/>
+          <v-icon
+            icon="fas fa-address-card"
+            class="mr-1 pa-2 ma-2"
+            size="x-small"
+          />
           Moments
         </v-list-item>
         <v-list-item @click="showLeaderboard">
-          <v-icon icon="fas fa-list-ul" class="mr-1 pa-2 ma-2" size="x-small"/>
+          <v-icon icon="fas fa-list-ul" class="mr-1 pa-2 ma-2" size="x-small" />
           Leaderboards
         </v-list-item>
         <v-list-item @click="showFriendsFoes">
-          <v-icon icon="fas fa-users" class="mr-1 pa-2 ma-2" size="x-small"/>
+          <v-icon icon="fas fa-users" class="mr-1 pa-2 ma-2" size="x-small" />
           Friends & Foes
         </v-list-item>
       </v-list>
@@ -114,64 +165,97 @@
 
     <v-main class="d-flex justify-center" :width="width">
       <div v-if="showDapperConnect">
-        <Dapper :user="user" @showUpcomingGames="showUpcomingEvents" @showMoments="showMoments"
-                @changeUsername="chooseUsername"/>
+        <Dapper
+          :user="user"
+          @showUpcomingGames="showUpcomingEvents"
+          @showMoments="showMoments"
+          @changeUsername="chooseUsername"
+        />
       </div>
       <div v-if="showMyEventComponent">
-        <MyEvents :user="user" @showUpcomingGames="showUpcomingEvents" @showGameView="showGame"/>
+        <MyEvents
+          :user="user"
+          @showUpcomingGames="showUpcomingEvents"
+          @showGameView="showGame"
+        />
       </div>
       <div v-if="showUpcomingEventComponent">
-        <UpcomingEvents :user="user" @showGameView="showGame"/>
+        <UpcomingEvents :user="user" @showGameView="showGame" />
       </div>
       <div v-if="showMomentsComponent">
-        <Moments :user="user" @showDapperView="showDapper"/>
+        <Moments :user="user" @showDapperView="showDapper" />
       </div>
       <div v-if="showLeaderboardComponent">
-        <Leaderboards :user="user"/>
+        <Leaderboards :user="user" />
       </div>
       <div v-if="showFriendsFoesComponent">
-        <FriendsFoes :user="user"/>
+        <FriendsFoes :user="user" />
       </div>
 
       <div v-if="showGameViewComponent">
-        <GameDetailView :user="user" :gameId="gameId" @showDapperView="showDapper"/>
+        <GameDetailView
+          :user="user"
+          :gameId="gameId"
+          @showDapperView="showDapper"
+        />
       </div>
-      <br>
+      <br />
     </v-main>
     <div class="mobile-menu-btn hidden-md-and-up">
       <v-btn @click.stop="drawer = !drawer" block>
-        <v-icon icon="fas fa-bars" class="mr-1 pa-2 ma-2" size="x-small"/>
+        <v-icon icon="fas fa-bars" class="mr-1 pa-2 ma-2" size="x-small" />
         Menu
       </v-btn>
     </div>
-    <v-dialog v-model="showUsername" width="auto" min-width="300"
-    >
+    <v-dialog v-model="showUsername" width="auto" min-width="300">
       <v-card class="pa-2">
-        <v-card-title><span v-if="!email"> Email &</span> Username:</v-card-title>
+        <v-card-title
+          ><span v-if="!email"> Email &</span> Username:</v-card-title
+        >
         <v-card-text>
-          <v-text-field v-if="!email" v-model="newEmail" label="Email" type="text"></v-text-field>
-          <v-text-field v-model="username" label="Username" type="text"></v-text-field>
-          <v-btn v-if="email" @click="saveUsername(username)" color="success" :disabled="!username">Save</v-btn>
-          <v-btn v-else @click="saveUsername(username)" color="success" :disabled="!newEmail&&!username">Save</v-btn>
+          <v-text-field
+            v-if="!email"
+            v-model="newEmail"
+            label="Email"
+            type="text"
+          ></v-text-field>
+          <v-text-field
+            v-model="username"
+            label="Username"
+            type="text"
+          ></v-text-field>
+          <v-btn
+            v-if="email"
+            @click="saveUsername(username)"
+            color="success"
+            :disabled="!username"
+            >Save</v-btn
+          >
+          <v-btn
+            v-else
+            @click="saveUsername(username)"
+            color="success"
+            :disabled="!newEmail && !username"
+            >Save</v-btn
+          >
         </v-card-text>
       </v-card>
     </v-dialog>
   </v-layout>
 </template>
 
-
 <script>
-import firebase from 'firebase/app';
-import 'firebase/firestore'
-import 'firebase/auth'
-import MyEvents from '@/components/MyEvents.vue'
-import UpcomingEvents from '@/components/UpcomingEvents.vue'
-import Moments from '@/components/Moments.vue'
-import Leaderboards from "@/components/Leaderboards.vue"
-import FriendsFoes from "@/components/FriendsFoes.vue"
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import MyEvents from "@/components/MyEvents.vue";
+import UpcomingEvents from "@/components/UpcomingEvents.vue";
+import Moments from "@/components/Moments.vue";
+import Leaderboards from "@/components/Leaderboards.vue";
+import FriendsFoes from "@/components/FriendsFoes.vue";
 
-import Dapper from '@/components/ConnectDapper.vue'
-import {useUserStore} from '@/store/app.js'
+import Dapper from "@/components/ConnectDapper.vue";
+import { useUserStore } from "@/store/app.js";
 import GameDetailView from "@/components/GameDetailView.vue";
 import db from "@/firebase/init";
 
@@ -203,121 +287,130 @@ export default {
       email: null,
       newEmail: null,
       username: null,
-    }
+    };
   },
   mounted() {
-    this.width = window.innerWidth
+    this.width = window.innerWidth;
     if (this.width > 800) {
-      this.width = 800
+      this.width = 800;
     }
-    this.user = useUserStore()
-    this.checkUser(this.user)
-
+    this.user = useUserStore();
+    if (this.user.loading === false) {
+      this.checkUser(this.user);
+    }
   },
   methods: {
     checkUser(user) {
-      this.user = user
-      this.profile = user.profile
-      this.email = this.user.user.email
+      this.user = user;
+      this.profile = user.profile;
+      this.email = this.user.user.email;
       if (!this.profile?.avatar) {
-        this.profile.avatar = `https://source.boringavatars.com/pixel/60/${this.profile?.dapperAddress | this.profile?.username}.png`
+        this.profile.avatar = `https://source.boringavatars.com/pixel/60/${
+          this.profile?.dapperAddress | this.profile?.username
+        }.png`;
       }
 
       if (this.user.user.uid && this.profile && !this.profile?.username) {
-        this.chooseUsername()
+        this.chooseUsername();
       } else if (this.user.user.uid && !this.user.user.email) {
-        this.chooseUsername()
-        this.username = this.profile?.username
+        this.chooseUsername();
+        this.username = this.profile?.username;
       } else {
-        this.username = this.profile?.username
+        this.username = this.profile?.username;
       }
     },
     chooseUsername() {
-      this.showUsername = true
+      this.showUsername = true;
     },
     async saveUsername(username) {
       const fields = {
         username: username,
-      }
-      db.collection('profiles')
+      };
+      db.collection("profiles")
         .doc(useUserStore().user.uid)
-        .set(fields, {merge: true})
+        .set(fields, { merge: true })
         .then(() => {
-          console.log('username updated')
-          useUserStore().profile.username = username
-          this.showUsername = false
-        }).catch((error) => {
-        console.log('error updating username', error)
-      })
-      if (this.newEmail) {
-        await firebase.auth().currentUser.updateEmail(this.newEmail).then(() => {
-          console.log('email updated')
-          this.email = this.newEmail
-          this.user.email = this.newEmail
-          this.showUsername = false
-        }).catch((error) => {
-          console.log('error updating email', error)
+          console.log("username updated");
+          useUserStore().profile.username = username;
+          this.showUsername = false;
         })
+        .catch((error) => {
+          console.log("error updating username", error);
+        });
+      if (this.newEmail) {
+        await firebase
+          .auth()
+          .currentUser.updateEmail(this.newEmail)
+          .then(() => {
+            console.log("email updated");
+            this.email = this.newEmail;
+            this.user.email = this.newEmail;
+            this.showUsername = false;
+          })
+          .catch((error) => {
+            console.log("error updating email", error);
+          });
       } else {
-        this.showUsername = false
+        this.showUsername = false;
       }
     },
     logout() {
-      firebase.auth().signOut()
+      firebase
+        .auth()
+        .signOut()
         .then(() => {
-          this.user = ''
-          console.log('logged out')
-          useUserStore().$reset()
-          this.$router.push('/')
+          this.user = "";
+          console.log("logged out");
+          useUserStore().$reset();
+          this.$router.push("/");
         })
         .catch((error) => {
-          console.log(error)
+          console.log(error);
         });
     },
 
     closeAll() {
-      this.drawer = false
-      this.showMyEventComponent = false
-      this.showDapperConnect = false
-      this.showMomentsComponent = false
-      this.showUpcomingEventComponent = false
-      this.showLeaderboardComponent = false
-      this.showFriendsFoesComponent = false
-      this.showGameViewComponent = false
+      this.drawer = false;
+      this.showMyEventComponent = false;
+      this.showDapperConnect = false;
+      this.showMomentsComponent = false;
+      this.showUpcomingEventComponent = false;
+      this.showLeaderboardComponent = false;
+      this.showFriendsFoesComponent = false;
+      this.showGameViewComponent = false;
     },
     showMyEvents() {
-      this.closeAll()
-      this.showMyEventComponent = true
+      this.closeAll();
+      this.showMyEventComponent = true;
     },
     showUpcomingEvents() {
-      this.closeAll()
-      this.showUpcomingEventComponent = true
+      this.closeAll();
+      this.showUpcomingEventComponent = true;
     },
     showMoments() {
-      this.closeAll()
-      this.showMomentsComponent = true
+      this.closeAll();
+      this.showMomentsComponent = true;
     },
     showLeaderboard() {
-      this.closeAll()
-      this.showLeaderboardComponent = true
+      this.closeAll();
+      this.showLeaderboardComponent = true;
     },
     showFriendsFoes() {
-      this.closeAll()
-      this.showFriendsFoesComponent = true
+      this.closeAll();
+      this.showFriendsFoesComponent = true;
     },
     showDapper() {
-      this.closeAll()
-      this.showDapperConnect = true
+      this.closeAll();
+      this.showDapperConnect = true;
     },
     showGame(gameId) {
-      console.log('show game', gameId)
-      this.gameId = gameId
-      this.closeAll()
-      this.showGameViewComponent = true
-    }
-
-  }
-}
+      console.log("show game", gameId);
+      this.gameId = gameId;
+      this.closeAll();
+      this.showGameViewComponent = true;
+    },
+  },
+};
 </script>
 
 <style>
